5d53f230b4d94c15d495dc20335c396366b936c4
Replit user <>
2023-03-22 02:26:35 +0000

    new try

--- list.c
+++ list.c
@@ -43,18 +43,21 @@ List * createList()
 
 void * firstList(List * list) 
 {
-  if (list -> head == NULL) return NULL;
+  if (list -> head == NULL || list == NULL ) return NULL;
   list -> current = list -> head;
     return list -> current -> data;
 }
 
 void * nextList(List * list) 
 {
-  if (list -> current == NULL) return NULL;
-    return list -> current -> next;
+  if (list -> current == NULL || list == NULL || list -> current -> next == NULL) return NULL;
+  list -> current = list -> current -> next;
+  return list -> current -> data;
 }
 
-void * lastList(List * list) {
+void * lastList(List * list)
+{
+  
     return NULL;
 }
 
